f0997f84bc80cb573b3b3ff77bebb8741cb59a24
buingocmay <ngocmay1597@gmail.com>
2019-05-14 23:50:06 +0700

    ckeditor

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   devise_for :users, controllers: {sessions: "sessions"}
   devise_for :admins, path: 'admin', skip: :registrations, controllers: {sessions: "admin/sessions"}
   namespace :admin do
