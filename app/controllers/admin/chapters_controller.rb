571f0419a80130a308e72f184080dfcf75a1578b
buingocmay <ngocmay1597@gmail.com>
2019-05-14 20:38:18 +0700

    done chapter

--- app/controllers/admin/chapters_controller.rb
+++ app/controllers/admin/chapters_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ChaptersController < Admin::AdminController
-	before_action :find_course, only: %i(new create)
+	before_action :find_course, only: %i(new create edit update)
+	before_action :find_chapter, only: %i(edit update destroy)
 
 	def show
 		
@@ -20,6 +21,28 @@ class Admin::ChaptersController < Admin::AdminController
   	end
 	end
 
+	def edit
+		@course_options = @course.unit.courses.map { |p| [p.name, p.id] }
+	end
+
+	def update
+		if @chapter.update chapter_params
+  		flash[:success] = "Cập nhật thành công"
+    else
+      render :edit
+    end
+	end
+
+	def destroy
+		@course = @chapter.course
+
+    if @chapter.destroy
+      flash[:success] = "Xóa thành công"
+    else
+      flash[:danger] = "Xóa thất bại"
+    end
+	end
+
 	private
 
 	def find_course
@@ -28,6 +51,14 @@ class Admin::ChaptersController < Admin::AdminController
 		return if @course
   	redirect_to admin_categories_path
   	flash[:danger] = "Không tìm thấy unit này"
+	end
+
+	def find_chapter
+		@chapter = Chapter.find_by id: params[:id]
+
+		return if @chapter
+  	redirect_to admin_categories_path
+  	flash[:danger] = "Không tìm thấy khóa học này"
 	end
 
 	def chapter_params
